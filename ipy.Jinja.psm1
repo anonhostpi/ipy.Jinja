4c31f55fe59b1620c395512baa57738acbc39966
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 16:49:40 -0700

    feat(module): add ipy.Jinja.psm1 scaffold with function stubs

---
+++ ipy.Jinja.psm1
@@ -0,0 +1,13 @@
+# ipy.Jinja.psm1 -- Loads vendored Jinja2+MarkupSafe into an IronPythonEmbedded engine
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+function Get-IpyJinjaZipPath {
+    <# WIP #>
+}
+
+function Install-IpyJinja {
+    <# WIP #>
+}
+
+Export-ModuleMember -Function Install-IpyJinja
