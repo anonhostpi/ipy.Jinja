dffa74156564f78a09cb402e7034573c45a7da96
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 16:49:55 -0700

    feat(module): implement Install-IpyJinja to load vendored zip into engine

--- ipy.Jinja.psm1
+++ ipy.Jinja.psm1
@@ -11,7 +11,17 @@ function Get-IpyJinjaZipPath {
 }
 
 function Install-IpyJinja {
-    <# WIP #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][object]$Engine,
+        [string]$ZipPath = (Get-IpyJinjaZipPath)
+    )
+    Write-Verbose "Loading ipy.Jinja from: $ZipPath"
+    $Engine.Add('/ipy/lib/site-packages', $ZipPath)
+    if (-not $Engine.Has('/ipy/lib/site-packages/jinja2/__init__.py')) {
+        throw "Install-IpyJinja: jinja2 package not found after loading zip. Verify zip contents."
+    }
+    Write-Verbose "Install-IpyJinja: jinja2 + markupsafe loaded successfully"
 }
 
 Export-ModuleMember -Function Install-IpyJinja
