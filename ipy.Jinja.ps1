84b1720ede317df9823f72c72b9cf479ec971809
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 17:44:31 -0700

    fix: use MarkupSafe source distribution instead of platform-specific wheel

--- ipy.Jinja.ps1
+++ ipy.Jinja.ps1
@@ -6,7 +6,7 @@
 #   Install-IpyJinja -Engine $engine
 
 $jinjaWheelUrl    = 'https://files.pythonhosted.org/packages/65/e0/eb35e762802015cab1ccee04e8a277b03f1d8e53da3ec3106882ec42558b/Jinja2-2.10.3-py2.py3-none-any.whl'
-$markupsafeWheelUrl = 'https://files.pythonhosted.org/packages/09/31/fe863b864cf3dfa11bce7a3bd41c4433d59b777ee0750b8d8c9a96f5ca98/MarkupSafe-1.1.1-cp34-cp34m-win_amd64.whl'
+$markupsafeWheelUrl = 'https://files.pythonhosted.org/packages/b9/2e/64db92e53b86efccfaea71321f597fa2e1b2bd3853d8ce658568f7a13094/MarkupSafe-1.1.1.tar.gz'
 
 # Patched Python source files (full content; loaded after wheel to overwrite originals)
 $patchedDebugPy    = @'
