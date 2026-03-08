c10d0f9d7898294b12c81f07f586d77b62a204fa
Blue Falcon <anon@hostpi.onmicrosoft.com>
2026-03-07 17:44:09 -0700

    fix: remove unnecessary script scope prefix from wheel URL vars

--- ipy.Jinja.ps1
+++ ipy.Jinja.ps1
@@ -5,8 +5,8 @@
 #   . "$PSScriptRoot/ipy.Jinja.ps1"
 #   Install-IpyJinja -Engine $engine
 
-$script:jinjaWheelUrl    = 'https://files.pythonhosted.org/packages/65/e0/eb35e762802015cab1ccee04e8a277b03f1d8e53da3ec3106882ec42558b/Jinja2-2.10.3-py2.py3-none-any.whl'
-$script:markupsafeWheelUrl = 'https://files.pythonhosted.org/packages/09/31/fe863b864cf3dfa11bce7a3bd41c4433d59b777ee0750b8d8c9a96f5ca98/MarkupSafe-1.1.1-cp34-cp34m-win_amd64.whl'
+$jinjaWheelUrl    = 'https://files.pythonhosted.org/packages/65/e0/eb35e762802015cab1ccee04e8a277b03f1d8e53da3ec3106882ec42558b/Jinja2-2.10.3-py2.py3-none-any.whl'
+$markupsafeWheelUrl = 'https://files.pythonhosted.org/packages/09/31/fe863b864cf3dfa11bce7a3bd41c4433d59b777ee0750b8d8c9a96f5ca98/MarkupSafe-1.1.1-cp34-cp34m-win_amd64.whl'
 
 # Patched Python source files (full content; loaded after wheel to overwrite originals)
 $script:patchedDebugPy    = @'
